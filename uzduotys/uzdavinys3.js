13ed920f9300f6650d066e1143f4c9e77920850a
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-03-08 14:59:11 +0200

    pap

--- uzduotys/uzdavinys3.js
+++ uzduotys/uzdavinys3.js
@@ -21,7 +21,7 @@ for(const sk of a){
 return naujas
 }
 
-console.log(biggest([1,2,3,2,3,5,6,7,8,9], 2))
+console.log('--->',biggest([1,2,3,2,3,5,6,7,8,9], 2))
 
 function palindromas(x){
 return ((x.split('').reverse()).join(''))===x ?"Taip": 
@@ -36,13 +36,16 @@ function nextLetter (x){
 x.split('');
 let text = [];
 for (let i=0;i<x.length;i++){
+    if (x[i].charCodeAt()===90){text.push(String.fromCharCode(65))}
+    else if (x[i].charCodeAt()===122){text.push(String.fromCharCode(97))}
+    else {
     const a = x[i].charCodeAt()+1;
-    text.push(String.fromCharCode(a))   
+    text.push(String.fromCharCode(a))}
 }
 return text.join("")
 }
 
-console.log(nextLetter("skaiste"));
+console.log(nextLetter("abraKaDraZZZzzz"));
 
 function count (a,b){
     let count = 0;
@@ -54,12 +57,41 @@ for(let i=0;i<a.length;i++){
 return count
 }
 
-console.log(count("aaaaaabbcc", "h"));
+console.log(count(1.2,3,4,5,6, "h"));
+
+
 
 function array (x){
 
-    
+    const flatArray = x.flat().sort((a,b)=>a-b);
 
+    let arr=[];
 
+    for(let i=0;i<flatArray.length;i++){
+        if(flatArray[i]===flatArray[i+1]){
+            arr.push(flatArray[i])
+        }
+    }
+    const arrFinal = arr.filter((i, index) => arr.indexOf(i) === index)
+
+  return arrFinal
 }
-console.log(array([1,2,3,3],[1,2,3]));
+
+
+
+
+console.log(array([[1,2,3,3],[2,5,3,4]]));
+
+console.log(array([[1,2,3,3],[2,5,3,4],[6,2,"f",5]]));
+console.log(array([[1,2,3,3],[2,5,3,4],[]]));
+console.log(array([[1,2,3,3],[2,5,3,4],[6,2,3,5,9],[2,5,5,10,1],[2,6,6,9]]));
+
+console.log("z".charCodeAt())
+
+
+
+
+   
+
+
+
