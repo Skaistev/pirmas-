f87636de873a1a7c0f7dc091fdf7ccf85fe250d9
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-03-18 13:43:57 +0200

    pap

--- uzduotys/uzdavinys4.js
+++ uzduotys/uzdavinys4.js
@@ -270,3 +270,12 @@ vartotojai4[x].amzius ++
 }
 console.log(vartotojai4);
 
+function capitalize (x){
+   
+   const b = x.split('');
+   const u = b[0].toUpperCase();
+   const s = x.slice(1,x.length)
+     return u+s
+ }
+
+ console.log(capitalize("trylika"));
